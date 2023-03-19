2d717170512e560486ac79b7c115d6e6839c8630
Kilesh Maharjan <kilesh.maharjan@introcept.co>
2023-03-19 19:05:38 +0545

    tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-blue-600'>Hello world</h1>
+      <p>Website Launching soon...</p>
+    </>
+  )
+}
+
+export default App
